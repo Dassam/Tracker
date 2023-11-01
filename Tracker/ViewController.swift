c0f447cfabf61ce993d98aa0ef807a8cb8f00b26
Dassam <dassam@yandex.ru>
2023-11-01 17:27:54 +0200

    Check Commit

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var star = "Day One"
         // Do any additional setup after loading the view.
     }
 
